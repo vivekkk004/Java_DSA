658efa5b6d50559442df1b6591c6da7474d76a0d
Vivek Muthe <vivekmuthe4@gmail.com>
2026-07-16 16:54:46 +0530

    Implement array rotation logic

--- RotateArray.java
+++ RotateArray.java
@@ -1,27 +1,22 @@
-public class RotateArray{
+public class RotateArray {
 
-    public static void RotateArr(int arr[]){
-        int frist = 0;
-        int last = arr.length-1;
+    public static void rotateRight(int arr[]) {
+        int last = arr[arr.length - 1];   // Store last element
 
-        while(frist<last){
-            int temp = arr[frist];
-            arr[frist] = arr[last];
-            arr[last]=temp;
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];          // Shift elements right
+        }
 
-            frist++;
-            last--;
+        arr[0] = last;                    // Place last at first
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {34, 34, 23, 5, 89};
+
+        rotateRight(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
     }
-    public static void main(String[] args){
-        int arr[] ={ 34, 34, 23, 5 , 89,};
-            RotateArr(arr);
-           for (int i=0; i<arr.length; i++){
-           
-            System.out.print(arr[i]+" ");
-           }
-            System.out.println();
-        }
-     
-    }
-    
+}
