c31dad0983edda7f484e4c22f7f74f333048d181
Vivek Muthe <vivekmuthe4@gmail.com>
2026-07-21 13:06:16 +0530

    String is palindrome or not

---
+++ String/ReverseString.java
@@ -0,0 +1,10 @@
+public class ReverseString {
+
+
+public static String Reverse(String str){
+    
+}
+    public static void main(String args[]{
+        
+    })
+}
--- String/palindrome.java
+++ String/palindrome.java
@@ -1,18 +1,18 @@
 public class palindrome{
 
-public static boolean isPalindrome(String str){
-    for(int i=0; i<str.length()/2; i++){
-        int n =str.length();
-        if (str.charAt(i) != str.charAt(n-1-i)){
-            // not a palindrome 
+    public static boolean isPalindrome(String str) {
+    int n = str.length();
+
+    for (int i = 0; i < n / 2; i++) {
+        if (str.charAt(i) != str.charAt(n - 1 - i)) {
             return false;
         }
     }
     return true;
 }
-
     public static void main(String args[]){
-   String str ="racecar";
-   System.out.println(isPalindrome(str));
+      String str ="Noon";
+  
+     System.out.println(isPalindrome(str));
     }
 }
