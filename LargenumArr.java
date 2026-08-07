bba8e9a2b30936ec6b86c70603c5803b4410fad6
Vivek Muthe <vivekmuthe4@gmail.com>
2026-08-07 18:56:24 +0530

    add string utility programs and update array algorithms for duplicate removal and second largest element tracking

--- LargenumArr.java
+++ LargenumArr.java
@@ -2,15 +2,20 @@ public class LargenumArr{
 
   public static void main(String args[]){
     int arr[] ={7,8,9,4,6,5};
-  int largest = arr[0];
+  int largest = Integer.MIN_VALUE;
+  int secondlargest = Integer.MIN_VALUE;
 
   for(int i=0; i<arr.length; i++){
     if(arr[i]>largest){
+       secondlargest = largest;
       largest = arr[i];
+    }else if( arr[i]<secondlargest && arr[i]!=largest){
+      secondlargest = arr[i];
     }
  
   }
-     System.out.print(largest);
+  System.out.println("Largest = "+largest);
+ System.out.println("Second Largest = " + secondlargest);
   }
 
 }
--- RemoveDup.java
+++ RemoveDup.java
@@ -1,30 +1,27 @@
 public class RemoveDup {
+       
+    public static int removeD(int arr[]) {
 
-    public static int removeDuplicates(int[] arr) {
+       int i = 0;
 
-        int i = 0;
-
-        for (int j = 1; j < arr.length; j++) {
-            if (arr[i] != arr[j]) {
-                arr[i + 1] = arr[j];
-                i++;
-            }
+       for(int j=1; j<arr.length; j++){
+        if(arr[j]!= arr[i]){
+            arr[i+1]= arr[j];
+            i++;
         }
-
-        return i + 1;
+       }
+       return(i+1);
     }
-
+    
     public static void main(String[] args) {
 
-        int[] arr = {1, 1, 2, 2, 3, 4, 4, 5};
+        int[] arr = {1, 1, 2, 2, 3, 4, 4,5,6};
+          int lenght = removeD(arr);
+        System.out.println("lenght :"+ lenght);
 
-        int length = removeDuplicates(arr);
-
-        System.out.println("Length = " + length);
-
-        System.out.print("Unique Elements: ");
-        for (int i = 0; i < length; i++) {
-            System.out.print(arr[i] + " ");
+        for(int i =0; i<lenght;i++){
+          System.out.print(arr[i]+" ");
         }
+     
     }
 }
